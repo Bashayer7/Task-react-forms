99286fbbf44170cd094d14c7a117b936fdc92485
Bashayer7 <bashayeralbaqshi57@gmail.com>
2022-01-16 14:24:05 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { useState } from 'react'
-import './App.css';
-import Form from './Components/Form'
-import HeaderAndTitle from './Components/HeaderAndTitle'
-import StudentsList from './Components/StudentsList';
+import { useState } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import HeaderAndTitle from "./Components/HeaderAndTitle";
+import StudentsList from "./Components/StudentsList";
 
 function App() {
+  const [studentsInfo, setStudentsInfo] = useState([]);
+  const [show, setShow] = useState(false);
 
-  const [studentsInfo, setStudentsInfo] = useState([])
-  const [show, setShow] = useState(false)
-
+  const addStudent = (newInfo) => {
+    console.log(newInfo);
+    setStudentsInfo([...studentsInfo, newInfo]);
+  };
+  const deleteStudent = (deleteStudent) => {
+    console.log(deleteStudent);
+    let l = studentsInfo.filter((student) => student.id != deleteStudent.id);
+  };
   return (
-
     <div className="App">
       <HeaderAndTitle showList={setShow} show={show} />
-      {show ? <StudentsList list={studentsInfo} /> : <Form />}
+      {show ? (
+        <StudentsList list={studentsInfo} />
+      ) : (
+        <Form addStudent={addStudent} />
+      )}
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,19 +1,46 @@
-import React from 'react'
-import SideInfo from './SideInfo'
+import React from "react";
+import SideInfo from "./SideInfo";
+import Student from "./Student";
 
+function Form(addStudent) {
+  //   let f = { name: "", lastName: "", phoneNumber: "", emailAddress: "" };
+  const hundleChange = (e) => {
+    setStudent({ ...Student, [e.target.name]: e.target.value });
+  };
+  const hundleSumbit = (e) => {
+    e.preventDefault();
+    addStudent(Student);
+  };
 
-function Form() {
+  return (
+    <div className="form-page">
+      <form className="student">
+        <input
+          name="name"
+          placeholder="enter your name"
+          onChange={hundleChange}
+        />
+        <input
+          name="lastName"
+          placeholder="enter your last name"
+          onChange={hundleChange}
+        />
+        <input
+          name="phoneNumber"
+          placeholder="enter your phone number"
+          onChange={hundleChange}
+        />
+        <input
+          name="emailAddress"
+          placeholder="enter your email"
+          onChange={hundleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
 
-    return (
-        <div className="form-page">
-
-            <form>
-                {/* to do :  create your form here  */}
-            </form>
-            <SideInfo />
-
-        </div>
-    )
+      <SideInfo />
+    </div>
+  );
 }
 
-export default Form
+export default Form;
